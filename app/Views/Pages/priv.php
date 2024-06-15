aec42139335b36613caf86f19369441e0ceb93df
Agustin Lago <agustinlago164@gmail.com>
2024-06-15 19:34:56 -0300

    .

--- app/Views/Pages/priv.php
+++ app/Views/Pages/priv.php
@@ -4,7 +4,7 @@
     <div class="terms-container">
         <main class="terms-and-conditions">
             <div class="terms-section">
-                <h2><i class="fa-solid fa-user-ninja"></i>  POLÍTICA DE PRIVACIDAD</h2>
+                <h2><i class="fa-solid fa-user-ninja"></i>  POLÍTICA De PRIVACIDAD</h2>
                 <p>El presente Política de Privacidad establece los términos en que Buen Dia usa y protege la información que es proporcionada por sus usuarios al momento de utilizar su sitio web. Esta compañía está comprometida con la seguridad de los datos de sus usuarios. Cuando le pedimos llenar los campos de información personal con la cual usted pueda ser identificado, lo hacemos asegurando que sólo se empleará de acuerdo con los términos de este documento. Sin embargo esta Política de Privacidad puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos revisar continuamente esta página para asegurarse que está de acuerdo con dichos cambios.</p>
             </div>
             
